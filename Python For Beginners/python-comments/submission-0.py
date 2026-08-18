741356b3c6b18e608afb2ee5c15c7234eadceca5
AKChalise <99538411+AKChalise@users.noreply.github.com>
2026-08-17 17:09:12 -0700

    Add: python-comments - submission-0

---
+++ Python For Beginners/python-comments/submission-0.py
@@ -0,0 +1,12 @@
+print("I belong here.")
+# print("Why am I here?")
+# print("Get me out of here!")
+print("I also belong here.")
+print("Me too!")
+
+
+
+
+
+ 
+# / this is slash vs \ this is backslash. / vs \
