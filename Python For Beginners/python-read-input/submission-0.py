a9d6ff941fbb212ba75eb331afa9badbf34db355
AKChalise <99538411+AKChalise@users.noreply.github.com>
2026-09-07 04:42:16 -0700

    Add: python-read-input - submission-0

---
+++ Python For Beginners/python-read-input/submission-0.py
@@ -0,0 +1,7 @@
+def read_and_print_input() -> None:
+    kane=input("Reading one line of text into a string: ")
+    print(kane)
+
+#Dumb mistake you forget to call the function
+read_and_print_input()
+read_and_print_input()
