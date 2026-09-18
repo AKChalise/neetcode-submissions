cda1e9338602ad2becc87e1dc1abf9215c818eaa
AKChalise <99538411+AKChalise@users.noreply.github.com>
2026-09-18 13:24:08 -0700

    Add: python-sorted-dict-basics - submission-0

---
+++ Python Coding Interviews/python-sorted-dict-basics/submission-0.py
@@ -0,0 +1,26 @@
+from typing import List
+from sortedcontainers import SortedDict
+def remove_keys(sorted_dict: SortedDict[str, int], keys: List[str]) -> SortedDict[str, int]:
+    for i in keys:
+        sorted_dict.pop(i)
+    return sorted_dict
+def get_values_before_target(sorted_dict: SortedDict[str, int], target: str) -> List[int]:
+    s=[]
+    for key in sorted_dict:
+        if key==target:
+            break
+        s.append(sorted_dict[key])
+    return s
+
+# Dumb mistake I made here did not see the edge case that after target is met after that the numbers have to be not counted in
+
+# do not modify below this line
+print(remove_keys(SortedDict({'Alice': 25, 'Bob': 30, 'Charlie': 35}), ['Bob']))
+print(remove_keys(SortedDict({'Alice': 25, 'Bob': 30, 'Charlie': 35, 'David': 40}), ['Bob', 'David']))
+print(remove_keys(SortedDict({'Alice': 25, 'Bob': 30, 'Charlie': 35, 'David': 40, 'Eve': 45}), ['Alice', 'Eve']))
+
+print(get_values_before_target(SortedDict({'Alice': 25, 'Bob': 30, 'Charlie': 35}), 'Bob'))
+print(get_values_before_target(SortedDict({'Alice': 25, 'Bob': 30, 'Charlie': 35, 'David': 40}), 'David'))
+print(get_values_before_target(SortedDict({'Alice': 25, 'Bob': 30, 'Charlie': 35, 'David': 40}), 'Charlie'))
+print(get_values_before_target(SortedDict({'Alice': 25, 'Bob': 30, 'Charlie': 35, 'David': 40}), 'Bob'))
+print(get_values_before_target(SortedDict({'Alice': 25, 'Bob': 30, 'Charlie': 35, 'David': 40}), 'Alice'))
