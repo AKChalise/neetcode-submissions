90927717c66867e90e24cb7dae336d05064c0e43
AKChalise <99538411+AKChalise@users.noreply.github.com>
2026-09-17 19:48:07 -0700

    Add: python-heap-n-smallest - submission-0

---
+++ Python Coding Interviews/python-heap-n-smallest/submission-0.py
@@ -0,0 +1,37 @@
+import heapq
+from typing import List
+
+
+def get_min_element(arr: List[int]) -> int:
+    k=heapq.nsmallest(1,arr)
+    return k[0]
+
+
+def get_min_4_elements(arr: List[int]) -> List[int]:
+    # Return elements in *increasing* order
+    k=heapq.nsmallest(4,arr)
+    return k
+
+
+def get_min_2_elements(arr: List[int]) -> List[int]:
+    # Return elements in *decreasing* order
+    s=[]
+    k=heapq.nsmallest(2,arr)
+    s.append(k[1])
+    s.append(k[0])
+    return s
+
+
+# do not modify below this line
+print(get_min_element([1, 2, 3]))
+print(get_min_element([3, 2, 1, 4, 6, 2]))
+print(get_min_element([1, 9, 7, 3, 2, 1, 4, 6, 2]))
+
+print(get_min_4_elements([1, 9, 7, 3, 2, 1, 4, 6, 2]))
+print(get_min_4_elements([1, 9, 7, 2, 1, 3, 2, 1, 4, 6, 2, 1]))
+print(get_min_4_elements([1, 9, 7, 2, 3, 2, 4, 6, 2]))
+
+print(get_min_2_elements([1, 9, 7, 3, 2, 1, 4, 6, 2]))
+print(get_min_2_elements([1, 9, 7, 2, 1, 3, 2, 1, 4, 6, 2, 1]))
+print(get_min_2_elements([1, 9, 7, 2, 3, 2, 4, 6, 2]))
+
